94f4e198243a38e821818c71fba4415b80d94bda
Santiago Soler <santisoler@fastmail.com>
2024-04-12 09:22:10 -0700

    Initial commit

---
+++ collage/_collage.py
@@ -0,0 +1,95 @@
+"""
+Functions to build the collage of contributors
+"""
+import sys
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+
+
+def get_contributors(organization, repository, github_username=None, github_token=None):
+    url = f"https://api.github.com/repos/{organization}/{repository}/contributors"
+    auth = None
+    if github_username is not None and github_token is not None:
+        auth = (github_username, github_token)
+    response = requests.get(url, auth=auth)
+    response.raise_for_status()
+    contributors = [entry["login"] for entry in response.json()]
+    return contributors
+
+
+def get_authors(organization, repo, authors_fname="AUTHORS.rst"):
+    # Get content of authors file in repo
+    url = (
+        f"https://raw.githubusercontent.com/{organization}/{repo}/main/{authors_fname}"
+    )
+    response = requests.get(url)
+    response.raise_for_status()
+    # Get github handle of authors
+    authors = re.findall("@([A-Za-z0-9-]+)", response.text)
+    authors.sort()
+    return authors
+
+
+def generate_figure(contributors):
+    # Rows and columns size
+    n_cols = 8
+    n_rows = int(np.ceil(len(contributors) / n_cols))
+    figsize = (4 * n_cols, 4 * n_rows)
+
+    # Make the figure
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=figsize)
+    for ax in axes.ravel():
+        ax.set_axis_off()
+    for contributor, ax in zip(contributors, axes.ravel()):
+        ax.imshow(plt.imread(f"https://github.com/{contributor}.png"))
+        ax.set(title=contributor)
+    return fig
+
+
+def main():
+    organization = "simpeg"
+    repos = ["simpeg", "discretize", "pydiso", "geoana", "aurora", "pymatsolver"]
+    ignore_contributors = ["quantifiedcode-bot", "thibaut-kobold", "cgohlke"]
+    include_contributors = ["leonfoks"]
+
+    # Get username and tokens from command line args
+    github_username = sys.argv[1]
+    github_token = sys.argv[2]
+
+    # Get contributors
+    contributors = []
+    for repo in repos:
+        contributors += get_contributors(
+            organization, repo, github_username, github_token
+        )
+
+    # Get authors
+    authors = []
+    for repo in repos:
+        try:
+            new_authors = get_authors(organization, repo)
+        except requests.HTTPError as e:
+            print(e)
+        else:
+            authors += new_authors
+
+    # Put them together
+    contributors = list(set(contributors + authors))
+
+    # Remove unwanted ones
+    contributors = [c for c in contributors if c not in ignore_contributors]
+
+    # Add required ones
+    contributors += include_contributors
+
+    # Sort them with a case-insensitive manner
+    contributors.sort(key=lambda s: s.lower())
+
+    fig = generate_figure(contributors)
+    fig.savefig("SimPEG_Contributors.png", dpi=72, bbox_inches="tight", pad_inches=0.1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ collage/cli.py
@@ -0,0 +1,118 @@
+import click
+import requests
+
+
+@click.command(context_settings={"help_option_names": ["-h", "--help"]})
+@click.argument("image", type=click.File("wb"))
+@click.option(
+    "--organization",
+    "-o",
+    default="simpeg",
+    show_default=True,
+    help="GitHub organization",
+)
+@click.option(
+    "--repositories",
+    "-o",
+    default="simpeg,discretize,pydiso,geoana,aurora,pymatsolver",
+    show_default=True,
+    help="List of repositories",
+)
+@click.option(
+    "--ignore",
+    default="quantifiedcode-bot",
+    show_default=True,
+    help="Ignore this contributors, don't add them to the collage",
+)
+@click.option(
+    "--extend-ignore",
+    default=None,
+    show_default=True,
+    help="Extend the list of ignored contributors",
+)
+@click.option(
+    "--include",
+    default=None,
+    show_default=True,
+    help="Include these contributors to the collage",
+)
+@click.option(
+    "--gh-username",
+    default=None,
+    show_default=True,
+    help="GitHub username to use with the token.",
+)
+@click.option(
+    "--gh-token",
+    default=None,
+    show_default=True,
+    help="GitHub token for the passed GitHub username.",
+)
+def cli(
+    image,
+    organization,
+    repositories,
+    ignore,
+    extend_ignore,
+    include,
+    gh_username,
+    gh_token,
+):
+    # ignore_contributors = ["quantifiedcode-bot", "thibaut-kobold", "cgohlke"]
+    # include_contributors = ["leonfoks"]
+
+    from ._collage import get_contributors, get_authors, generate_figure  # lazy imports
+
+    # Sanitize inputs
+    repositories = [r.strip() for r in repositories.split(",")]
+
+    if ignore is None:
+        ignore = []
+    else:
+        ignore = [c.strip() for c in ignore.split(",")]
+    if extend_ignore is None:
+        extend_ignore = []
+    else:
+        extend_ignore = [c.strip() for c in extend_ignore.split(",")]
+    ignore_contributors = list(set(ignore + extend_ignore))
+
+    if include is None:
+        include = []
+    else:
+        include = [c.strip() for c in include.split(",")]
+
+    # Get contributors
+    contributors = []
+    for repo in repositories:
+        contributors += get_contributors(organization, repo, gh_username, gh_token)
+
+    # Get authors
+    authors = []
+    for repo in repositories:
+        try:
+            new_authors = get_authors(organization, repo)
+        except requests.HTTPError as e:
+            click.echo(e, err=True)
+        else:
+            authors += new_authors
+
+    # Put them together
+    contributors = list(set(contributors + authors))
+
+    # Remove unwanted ones
+    contributors = [c for c in contributors if c not in ignore_contributors]
+
+    # Add required ones
+    contributors += include
+
+    # Sort them with a case-insensitive manner
+    contributors.sort(key=lambda s: s.lower())
+
+    click.echo("Collected contributors:")
+    click.echo("-----------------------")
+    for contributor in contributors:
+        click.echo(f"- {contributor}")
+
+    fig = generate_figure(contributors)
+    fig.savefig(image, dpi=72, bbox_inches="tight", pad_inches=0.1)
+    click.echo(f"Done! 🎉 Collage image saved in {image}")
